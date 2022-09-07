4c2d8e363d06f1030384c7f8b89c3dc29d208abf
Faithful <olalerufaithful@gmail.com>
2022-09-07 19:13:12 +0100

    linear search done

--- src/main/java/com/faithfulolaleru/SearchAlgorithm/SearchAlgorithms.java
+++ src/main/java/com/faithfulolaleru/SearchAlgorithm/SearchAlgorithms.java
@@ -1,4 +1,20 @@
 package com.faithfulolaleru.SearchAlgorithm;
 
 public class SearchAlgorithms {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 10, 23, 11};
+        SearchAlgorithms.linearSearch(arr, 10);
+    }
+
+    public static int linearSearch(int arr[], int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == value) {
+                System.out.println("Element found at index: " + i);
+                return i;
+            }
+        }
+        System.out.println("Element " + value + " not found");
+        return -1;
+    }
 }
