a3c05f5d0e917d199649aced76fa642e7832ddd5
Faithful <olalerufaithful@gmail.com>
2022-09-07 19:48:24 +0100

    binary search done

--- src/main/java/com/faithfulolaleru/SearchAlgorithm/SearchAlgorithms.java
+++ src/main/java/com/faithfulolaleru/SearchAlgorithm/SearchAlgorithms.java
@@ -5,6 +5,7 @@ public class SearchAlgorithms {
     public static void main(String[] args) {
         int[] arr = {1, 3, 2, 10, 23, 11};
         SearchAlgorithms.linearSearch(arr, 10);
+        SearchAlgorithms.binarySearch(arr, 23);
     }
 
     public static int linearSearch(int arr[], int value) {
@@ -16,5 +17,30 @@ public class SearchAlgorithms {
         }
         System.out.println("Element " + value + " not found");
         return -1;
+    }
+
+    public static int binarySearch(int arr[], int value) {
+        int start = 0, end = arr.length -1;
+        int middle = (start + end)/2;
+        // System.out.println(start + " " + middle + " " + end);
+
+        while(arr[middle] != value  && start <= end) {   //  &&  to avoid infinite loop
+            if(value < arr[middle]) {  // use first half
+                end = middle - 1;
+            } else {   // use 2nd half
+                start = middle + 1;
+            }
+            middle = (start + end)/2;   // calculate new middle
+            // System.out.println(start + " " + middle + " " + end);
+        }
+
+
+        if(arr[middle] == value){
+            System.out.println("Element found at index: " + middle);
+            return middle;
+        } else {
+            System.out.println("Element " + value + " not found");
+            return -1;
+        }
     }
 }
