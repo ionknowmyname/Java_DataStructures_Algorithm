7f6e7cb1e077ba7ed9ff984be75f4e3d0b1b2571
Faithful <olalerufaithful@gmail.com>
2022-09-23 13:31:36 +0100

    failed interview question

---
+++ src/main/java/com/faithfulolaleru/INTERVIEWS/ConnectingToDB.java
@@ -0,0 +1,64 @@
+package com.faithfulolaleru.INTERVIEWS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ConnectingToDB {
+
+    private static final java.util.logging.Logger log = java.util.logging.Logger.getLogger(ConnectingToDB.class.getName());
+
+    private static HttpURLConnection conn;
+
+    public static void main(String[] args) {
+
+        BufferedReader reader;
+        String line;
+        StringBuilder responseContent = new StringBuilder();
+        try{
+            URL url = new URL("https://jsonmock.hackerrank.com/api/moviesdata/search/?Title=Gi");
+            conn = (HttpURLConnection) url.openConnection();
+
+            // Request setup
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(5000);// 5000 milliseconds = 5 seconds
+            conn.setReadTimeout(5000);
+
+            // Test if the response from the server is successful
+            int status = conn.getResponseCode();
+
+            if (status >= 300) {
+                reader = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                while ((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            }
+            else {
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            }
+            log.info("response code: " + status);
+            System.out.println(responseContent.toString());
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            conn.disconnect();
+        }
+
+    }
+
+    private String returnURLwithQuery(String query) {
+        String baseUrl = "https://jsonmock.hackerrank.com/api/moviesdata/search/?Title=";
+        return baseUrl + query;
+    }
+}
