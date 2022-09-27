d4d301d6469c79c733293887709b2b76a987ebde
Faithful <olalerufaithful@gmail.com>
2022-09-27 14:30:19 +0100

    breadth first search solution to single source shortest path problem with adjacency matrix

--- src/main/java/com/faithfulolaleru/Graphs/GraphWithAdjacencyMatrix.java
+++ src/main/java/com/faithfulolaleru/Graphs/GraphWithAdjacencyMatrix.java
@@ -1,6 +1,7 @@
 package com.faithfulolaleru.Graphs;
 
 import com.faithfulolaleru.base.GraphNodeAdjacencyMatrix;
+import com.faithfulolaleru.base.GraphNodeSSSPP;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -9,13 +10,23 @@ import java.util.Stack;
 public class GraphWithAdjacencyMatrix {
 
     ArrayList<GraphNodeAdjacencyMatrix> nodeList = new ArrayList<GraphNodeAdjacencyMatrix>();
+    ArrayList<GraphNodeSSSPP> nodeList2 = new ArrayList<GraphNodeSSSPP>();
+
     int[][] adjacencyMatrix;
+    int[][] adjacencyMatrix2;
 
 
 
+    /*
     public GraphWithAdjacencyMatrix(ArrayList<GraphNodeAdjacencyMatrix> nodeList) {
         this.nodeList = nodeList;
         adjacencyMatrix = new int[nodeList.size()][nodeList.size()];   // make a 2D array with the number of nodes
+    }
+    */
+
+    public GraphWithAdjacencyMatrix(ArrayList<GraphNodeSSSPP> nodeList2) {
+        this.nodeList2 = nodeList2;
+        adjacencyMatrix2 = new int[nodeList2.size()][nodeList2.size()];
     }
 
 
@@ -48,6 +59,8 @@ public class GraphWithAdjacencyMatrix {
 
         // Graph for Topological Sort
 
+        /*
+
         ArrayList<GraphNodeAdjacencyMatrix> nodeList = new ArrayList<GraphNodeAdjacencyMatrix>();
         nodeList.add(new GraphNodeAdjacencyMatrix("A", 0));
         nodeList.add(new GraphNodeAdjacencyMatrix("B", 1));
@@ -70,6 +83,34 @@ public class GraphWithAdjacencyMatrix {
         System.out.print(g.toString());
 
         g.topologicalSort();
+
+        */
+
+
+        // Graph for SSSPP
+
+        ArrayList<GraphNodeSSSPP> nodeList = new ArrayList<GraphNodeSSSPP>();
+        nodeList.add(new GraphNodeSSSPP("A", 0));
+        nodeList.add(new GraphNodeSSSPP("B", 1));
+        nodeList.add(new GraphNodeSSSPP("C", 2));
+        nodeList.add(new GraphNodeSSSPP("D", 3));
+        nodeList.add(new GraphNodeSSSPP("E", 4));
+        nodeList.add(new GraphNodeSSSPP("F", 5));
+        nodeList.add(new GraphNodeSSSPP("G", 6));
+
+        GraphWithAdjacencyList g = new GraphWithAdjacencyList(nodeList);
+        g.addUndirectedEdgeSSSPP(0, 1);
+        g.addUndirectedEdgeSSSPP(0, 2);
+        g.addUndirectedEdgeSSSPP(1, 3);
+        g.addUndirectedEdgeSSSPP(1, 6);
+        g.addUndirectedEdgeSSSPP(2, 3);
+        g.addUndirectedEdgeSSSPP(2, 4);
+        g.addUndirectedEdgeSSSPP(3, 5);
+        g.addUndirectedEdgeSSSPP(4, 5);
+        g.addUndirectedEdgeSSSPP(5, 6);
+        System.out.print(g.toString());
+
+        g.BFSForSSSPP(nodeList.get(0));
     }
 
 
@@ -220,6 +261,66 @@ public class GraphWithAdjacencyMatrix {
 
         while(!stack.isEmpty()) {
             System.out.print(stack.pop().name + " ");
+        }
+    }
+
+
+
+
+    ////////////////////////////////////////////////////
+    /////  FOR SSSPP Single Source Shortest Path  //////
+    ////////////////////////////////////////////////////
+
+
+
+    public void addUndirectedEdgeSSSPP(int i, int j) {
+        GraphNodeSSSPP first = nodeList2.get(i);
+        GraphNodeSSSPP second = nodeList2.get(j);
+
+        first.neighbors.add(second);
+        second.neighbors.add(first);
+
+    }
+
+    public ArrayList<GraphNodeSSSPP> getNeighborsSSSPP(GraphNodeSSSPP node) {
+        ArrayList<GraphNodeSSSPP> neighbors = new ArrayList<GraphNodeSSSPP>();
+        int nodeIndex = node.index;
+
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            if(adjacencyMatrix[nodeIndex][i] == 1) {   // that means they adjacent i.e there is an edge connecting the nodes
+                neighbors.add(nodeList2.get(i));
+            }
+        }
+
+        return neighbors;
+    }
+
+    public static void pathPrint(GraphNodeSSSPP node) {
+        if (node.parent != null) {
+            pathPrint(node.parent);
+        }
+        System.out.print(node.name + " ");
+    }
+
+    public void BFSForSSSPP(GraphNodeSSSPP node) {
+        LinkedList<GraphNodeSSSPP> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            GraphNodeSSSPP currentNode = queue.remove(0);
+            currentNode.isVisited = true;
+            System.out.print("Printing path for node " + currentNode.name + ": ");
+            pathPrint(currentNode);
+            System.out.println();
+
+            ArrayList<GraphNodeSSSPP> neighbors = getNeighborsSSSPP(currentNode);
+            for (GraphNodeSSSPP neighbor : neighbors) {
+                if(!neighbor.isVisited) {   // if adjacent nodes are not visited, add to queue
+                    queue.add(neighbor);
+                    neighbor.isVisited = true;
+                    neighbor.parent = currentNode;  // set adjacent nodes' parent to current node
+                }
+            }
         }
     }
 
--- src/main/java/com/faithfulolaleru/base/GraphNodeSSSPP.java
+++ src/main/java/com/faithfulolaleru/base/GraphNodeSSSPP.java
@@ -6,6 +6,8 @@ public class GraphNodeSSSPP extends GraphNode {
 
     public GraphNodeSSSPP parent;
 
+
+    // this neighbors array is only for adjacency list, for adjacency matrix, we use neighbors in its class
     public ArrayList<GraphNodeSSSPP> neighbors = new ArrayList<GraphNodeSSSPP>();
 
 
