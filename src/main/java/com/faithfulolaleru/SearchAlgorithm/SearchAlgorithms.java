19869c29183f5ce6ac0329dc366bb8f2f29294a8
Faithful Olaleru <faithfulolaleru09@gmail.com>
2023-02-18 14:35:41 +0100

    binary search recursive

--- src/main/java/com/faithfulolaleru/SearchAlgorithm/SearchAlgorithms.java
+++ src/main/java/com/faithfulolaleru/SearchAlgorithm/SearchAlgorithms.java
@@ -1,11 +1,13 @@
 package com.faithfulolaleru.SearchAlgorithm;
 
+import java.util.Arrays;
+
 public class SearchAlgorithms {
 
     public static void main(String[] args) {
         int[] arr = {1, 3, 2, 10, 23, 11};
         SearchAlgorithms.linearSearch(arr, 10);
-        SearchAlgorithms.binarySearch(arr, 23);
+        SearchAlgorithms.binarySearchIterative(arr, 23);
     }
 
     public static int linearSearch(int arr[], int value) {
@@ -19,7 +21,9 @@ public class SearchAlgorithms {
         return -1;
     }
 
-    public static int binarySearch(int arr[], int value) {
+    public static int binarySearchIterative(int arr[], int value) {
+
+        Arrays.sort(arr);
         int start = 0, end = arr.length -1;
         int middle = (start + end)/2;
         // System.out.println(start + " " + middle + " " + end);
@@ -38,6 +42,30 @@ public class SearchAlgorithms {
         if(arr[middle] == value){
             System.out.println("Element found at index: " + middle);
             return middle;
+        } else {
+            System.out.println("Element " + value + " not found");
+            return -1;
+        }
+    }
+
+    public static int binarySearchRecursive(int arr[], int value, int start, int end) {
+
+        Arrays.sort(arr);
+        int middle = (start + end)/2;
+
+
+        if(start <= end) {   // to avoid infinite loop; can also use if
+            if(arr[middle] == value) {
+                System.out.println("Element found at index: " + middle);
+                return middle;
+            }
+
+            if(value < arr[middle]) {  // use first half
+                return binarySearchRecursive(arr, value, start, middle - 1);
+            } else {   // use 2nd half
+                return binarySearchRecursive(arr, value, middle + 1, end);
+            }
+
         } else {
             System.out.println("Element " + value + " not found");
             return -1;
