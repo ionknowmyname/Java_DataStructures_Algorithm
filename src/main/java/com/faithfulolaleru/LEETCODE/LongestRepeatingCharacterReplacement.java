eddd10da803f747b45c9534c9f4030d9da783e6e
Faithful <faithfulolaleru09@gmail.com>
2023-03-30 21:56:08 +0100

    longest repeating character replacement leetcode

---
+++ src/main/java/com/faithfulolaleru/LEETCODE/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,45 @@
+package com.faithfulolaleru.LEETCODE;
+
+public class LongestRepeatingCharacterReplacement {
+
+
+
+    // 424. Longest Repeating Character Replacement
+    // sliding window
+    public int characterReplacement(String s, int k) {
+        int N = s.length();
+        int start = 0, maxLength = 0, maxCount = 0;
+        int[] charCounts = new int[26]; // each index keeps track of the count of each character
+
+
+        for (int end = 0; end < N; end++) {
+            charCounts[s.charAt(end) - 'A']++;  // increment count
+            // i.e if charAt(end) is C, increase charCounts[C - A] by 1, which is charCounts[2]
+            // ASCII code for A is 65, C is 67, so 65-67 which is 2, charCounts[2]
+            // also if you follow, index 0, of charCounts would be A, 1 would be B, 2 would be C
+
+            int currentCharCount = charCounts[s.charAt(end) - 'A'];
+            maxCount = Math.max(maxCount, currentCharCount);
+            // updating the max count, which would be the most number of times a letter appears
+
+            while(end - start - maxCount + 1 > k) {
+                // end - start is regular window, maxCount is count of letters that are same
+                // so (end - start - maxCount) accounts for letters that are different from the letter
+                // that owns maxCount & that needs tp be changed to find max repeating substring within our window
+                // + 1 is coz we adding a new letter on each loop
+
+                // so if we enter this while, it means we are at more than no. of times we can exchange characters
+                // so we keep moving the left pointer forward until we are less than k
+
+                charCounts[s.charAt(start) - 'A']--;
+                // first we reduce the count of the letter at the left pointer (start)
+
+                start++; // then you move the actual left pointer forward
+            }
+
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
+    }
+}
