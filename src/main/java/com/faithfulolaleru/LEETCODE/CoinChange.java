c49ed7c4aaeadcf19364effb0a0294c1b5be6616
Faithful <faithfulolaleru09@gmail.com>
2023-04-15 19:42:48 +0100

    Coin Change Greedy Programming

---
+++ src/main/java/com/faithfulolaleru/LEETCODE/CoinChange.java
@@ -0,0 +1,92 @@
+package com.faithfulolaleru.LEETCODE;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CoinChange {
+
+
+    public static void main(String[] args) {
+        // System.out.println(coinChange3(new int[] { 1, 2, 5 }, 11));
+        System.out.println(coinChange3(new int[] { 2 }, 3));
+        // System.out.println(coinChange2(new int[] { 186,419,83,408 }, 6249));
+    }
+
+    // my solution
+    // greedy algorithm
+    // timed out
+    public static int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        int biggestCoin; // = coins[coins.length - 1];
+        List<Integer> result = new ArrayList<>();
+
+        while(amount > 0) {
+            for(int i = coins.length - 1; i >= 0; i--) {
+                biggestCoin = coins[i];
+                if(amount > biggestCoin) {
+                    amount = amount - biggestCoin;
+                    result.add(biggestCoin);
+                }
+            }
+        }
+
+        return result.size();
+    }
+
+    // online solution
+    // greedy alogrithm
+    // failed some tests, but very good solution too
+    public static int coinChange2(int[] coins, int amount) {
+        Arrays.sort(coins);
+        List<Integer> result = new ArrayList<>();
+
+        for(int i = coins.length - 1; i >= 0; i--) {
+            while(amount >= coins[i]) {
+                amount -= coins[i];
+                result.add(coins[i]);
+            }
+        }
+
+        if(amount > 0) return -1;
+        else return result.size();
+    }
+
+
+    /*
+    *   best greedy approach bt fails some tests
+    *   greedy programming can never solve completely, use Dynamic Programming
+    *
+    *   this is coz, when you start from the biggest numbers, it keeps using the biggest numbers till it cant anymore
+    *   then it goes to the next smaller, bt if at that point the remaining amount is too small for the available coin
+    *   numbers, it'll return -1 E.g 21  & { 2, 3, 5 }, it'll keep taking 5, 4 times till its 20, and not find 1 to make
+    *   it 21 and therefore coz amount doesn't come back to 0, it'll return -1; whereas it coulda taken 5, 3 times, and
+    *   3, 2 times to make 21; only Dynamic Programming can do that;
+    * */
+    public static int coinChange3(int[] coins, int amount) {
+        Arrays.sort(coins);
+        int index = coins.length - 1;
+        List<Integer> result = new ArrayList<>();
+
+        while(true && index >= 0) {
+            int coinValue = coins[index];
+            index--;
+            int maxAmount = (amount / coinValue ) * coinValue;
+
+            if(maxAmount > 0) {
+                result.add(amount / coinValue);
+                amount -= maxAmount;
+            }
+
+            // if(amount == 0) break;
+        }
+
+        if(amount > 0) return -1;
+
+        int toReturn = 0;
+        for (int i = 0; i < result.size(); i++) {
+            toReturn += result.get(i);
+        }
+        return toReturn;
+    }
+}
