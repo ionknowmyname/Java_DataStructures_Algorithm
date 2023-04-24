8284a20b0b9dc4f9e475c5ecdd452f7a5a22f23b
Faithful <faithfulolaleru09@gmail.com>
2023-04-24 18:55:50 +0100

    longest palindromic subsequence divive & conquer

--- src/main/java/com/faithfulolaleru/LEETCODE/LongestPalindromicSubstring.java
+++ src/main/java/com/faithfulolaleru/LEETCODE/LongestPalindromicSubstring.java
@@ -4,18 +4,20 @@ import java.nio.charset.StandardCharsets;
 
 public class LongestPalindromicSubstring {
 
-    // 5. Longest Palindromic Substring/Subsequence
+    // 5. Longest Palindromic Substring (substring is continuous)
 
     public static void main(String[] args) {
 
-        System.out.println(longestPalindrome("racecar"));
-        System.out.println(longestPalindrome("abba"));
-        System.out.println(longestPalindrome("aaaabba"));
+        System.out.println(longestPalindromeSubString("racecar"));
+        System.out.println(longestPalindromeSubString("abba"));
+        System.out.println(longestPalindromeSubString("aaaabba"));
+
+        System.out.println(longestPalindromeSubSequence("ELRMENMET"));
     }
 
 
 
-    public static String longestPalindrome(String s) {
+    public static String longestPalindromeSubString(String s) {
         if(s == null || s.length() < 1) return "";
 
         int start = 0, end = 0;
@@ -51,5 +53,37 @@ public class LongestPalindromicSubstring {
         }
 
         return right - left - 1;
+    }
+
+
+    // Longest Palindromic Subsequence (subsequence doesnt have to be continuous just in place in order)
+    // E.g "EMEME" is a valid Palindromic Subsequence for "ELRMENMET" even though its not continuous
+    // solving using divide & conquer algorithm
+    private static int longestPalindromeSubSequence(String string, int startIndex, int endIndex) {
+        // pick first and last elements, if they match, it'll be 2 + f(2, n-1), i.e 2nd element and 2nd to the last
+        // element; if they don't match, we can either pick 2nd element to compare with last element, or 1st element
+        // with 2nd to last element i.e 0 + f(1, n-1) or 0 + (2, n); then get max of the 3 options
+
+
+        // base conditions
+        if(startIndex > endIndex) return 0; // we've reached & passed string middle
+
+        if(startIndex == endIndex) return 1; // a palindrone with 1 character, it'll be the middle character
+
+        int count1 = 0;
+
+        // chars at start and end match
+        if(string.charAt(startIndex) == string.charAt(endIndex)) {
+            count1 = 2 + longestPalindromeSubSequence(string, startIndex + 1, endIndex - 1);
+        }
+
+        int count2 = longestPalindromeSubSequence(string, startIndex + 1, endIndex);
+        int count3 = longestPalindromeSubSequence(string, startIndex, endIndex - 1);
+
+        return Math.max(count1, Math.max(count2, count3));
+    }
+
+    public static int longestPalindromeSubSequence(String string) {
+        return longestPalindromeSubSequence(string, 0, string.length() - 1);
     }
 }
